83b5ec9ecc96430f3a7e441dddc8c71f22723e48
Sule Tekin <suleetekin99@gmail.com>
2024-03-08 00:16:30 -0600

    typing animation incorporated

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
-// script.js
+window.onload = function() {
+    typeTextWithCursor('Sule Tekin', 'Computer Science Graduate', 'typeEff', 100);
+  };
+  
+  function typeTextWithCursor(name, additionalText, targetElementId, speed) {
+    var i = 0;
+    var txt1 = name;
+    var txt2 = additionalText;
+    var typingSpeed = speed || 50; // Default typing speed
+  
+    function typeWriter() {
+      if (i < txt1.length) {
+        document.getElementById(targetElementId).innerHTML += txt1.charAt(i);
+        i++;
+        setTimeout(typeWriter, typingSpeed);
+      } else {
+        // After typing the name, move to a new line and start typing the additional text after a delay
+        document.getElementById(targetElementId).innerHTML += "<br>" + "<br>"; // Move to a new line
+        setTimeout(startTypingAdditionalText, 1000); // Pause for 1 second before typing the second text
+      }
+    }
+  
+    function startTypingAdditionalText() {
+      // Add a blinking cursor on the left
+      document.getElementById(targetElementId).innerHTML += "<span id='cursor'>&nbsp;</span>";
+      setTimeout(function() {
+        // After 1 second, remove the cursor and start typing the additional text
+        document.getElementById("cursor").remove();
+        typeWriter2();
+      }, 1000);
+    }
+  
+    function typeWriter2() {
+      document.getElementById(targetElementId).innerHTML += txt2.charAt(i - txt1.length);
+      i++;
+      if (i <= txt1.length + txt2.length) {
+        setTimeout(typeWriter2, typingSpeed);
+      }
+    }
+  
+    // Start typing when the function is called
+    typeWriter();
+  }
+  
+
+
 
 function showRelatedCoursework() {
     var backButton = document.getElementById("back");
@@ -9,8 +54,8 @@ function showRelatedCoursework() {
     document.getElementById("github").style.display = "none";
     document.getElementById("linkedin").style.display = "none";
 
-    document.getElementById("bioName").style.display = "none";
-    document.getElementById("bioTitle").style.display = "none";
+    document.getElementById("typeEff").style.display = "none";
+    document.getElementById("bioMe").style.display = "none";
 
     document.getElementById("links").style.display = "none";
 
@@ -24,7 +69,7 @@ function showRelatedCoursework() {
 
     document.getElementById("back").style.display = "block";
 
-    document.getElementById("bioMe").style.display = "none";
+    document.getElementById("typeEff").style.display = "none";
 
     backButton.onclick = function() {
         showShortBio();
@@ -58,3 +103,4 @@ function showShortBio() {
         showRelatedCoursework();
     };
 }
+
